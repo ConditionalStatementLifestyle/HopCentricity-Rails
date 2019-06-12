422b7187b83d97218030f3d188ad2d76da491dd7
github username <github email address>
2019-06-11 22:28:23 -0700

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Beer.destroy_all
+
+s1 = Scraper.new
+
+s1.relative_path = '/Users/don/Development/HomeProjects/HopCentricity/HopCentricity-Rails/authed.html'
+s1.loadRelativePage
+
+s1.relative_path = '/Users/don/Development/HomeProjects/HopCentricity/HopCentricity-Rails/authed2.html'
+s1.loadRelativePage
+
+s1.relative_path = '/Users/don/Development/HomeProjects/HopCentricity/HopCentricity-Rails/authed3.html'
+s1.loadRelativePage
