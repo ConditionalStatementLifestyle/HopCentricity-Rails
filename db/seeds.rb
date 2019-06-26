e7dd3c4db71c96460b5f876d844083530be5e5fc
github username <github email address>
2019-06-26 15:15:02 -0700

    changed relative paths

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@ Beer.destroy_all
 
 s1 = Scraper.new
 
-s1.relative_path = '../authed.html'
+s1.relative_path = 'authed.html'
 s1.loadRelativePage
 
-s1.relative_path = '../authed2.html'
+s1.relative_path = 'authed2.html'
 s1.loadRelativePage
 
-s1.relative_path = '../authed3.html'
+s1.relative_path = 'authed3.html'
 s1.loadRelativePage
